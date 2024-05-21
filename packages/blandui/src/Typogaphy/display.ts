9f46557c1fc61a2c57e1e1c39585b2ad7fb86493
Ludovic Lerus <lerus_l@epitech.eu>
2024-05-21 20:09:24 +0100

    feat(blandui): First version of the plugin added

---
+++ packages/blandui/src/Typogaphy/display.ts
@@ -0,0 +1,34 @@
+export default {
+	'.display-sm': {
+		fontSize:   '3.3125rem',
+		fontWeight: '400',
+		lineHeight: '3.5rem',
+	},
+	'.display-sm-bold': {
+		fontSize:   '3.3125rem',
+		fontWeight: '600',
+		lineHeight: '3.5rem',
+	},
+
+	'.display-md': {
+		fontSize:   '4.1875rem',
+		fontWeight: '400',
+		lineHeight: '4.5rem',
+	},
+	'.display-md-bold': {
+		fontSize:   '4.1875rem',
+		fontWeight: '600',
+		lineHeight: '4.5rem',
+	},
+
+	'.display-lg': {
+		fontSize:   '5.1875rem',
+		fontWeight: '400',
+		lineHeight: '5.5rem',
+	},
+	'.display-lg-bold': {
+		fontSize:   '5.1875rem',
+		fontWeight: '600',
+		lineHeight: '5.5rem',
+	},
+};
---
+++ packages/blandui/src/Typogaphy/index.ts
@@ -0,0 +1,11 @@
+import display from './display';
+import heading from './heading';
+import title from './title';
+import body from './body';
+
+export default {
+	...display,
+	...heading,
+	...title,
+	...body,
+};
