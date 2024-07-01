1c37f94cfa3921360e1af5c67c07096d700c9a31
Ludovic Lerus <lerus_l@epitech.eu>
2024-07-01 12:51:24 +0300

    feat(Button): More classes to be able to customize more the Button component

--- packages/blandui/src/Color/borderColor.ts
+++ packages/blandui/src/Color/borderColor.ts
@@ -19,4 +19,40 @@ export default {
 
 	brand:   colors.carbon[1000],
 	inverse: colors.gray[0],
+
+	// BUTTON - BRAND
+	'btn-brand-primary-focused':   colors.carbon[300],
+	'btn-brand-secondary-focused': colors.carbon[300],
+	'btn-brand-ghost-focused':     colors.carbon[300],
+
+	'btn-brand-stroke-rest':     colors.carbon[100],
+	'btn-brand-stroke-focused':  colors.carbon[300],
+	'btn-brand-stroke-disabled': colors.gray[100],
+
+	// BUTTON - SUCCESS
+	'btn-success-primary-focused':   colors.green[200],
+	'btn-success-secondary-focused': colors.green[200],
+	'btn-success-ghost-focused':     colors.green[200],
+
+	'btn-success-stroke-rest':     colors.green[500],
+	'btn-success-stroke-focused':  colors.green[200],
+	'btn-success-stroke-disabled': colors.gray[100],
+
+	// BUTTON - DANGER
+	'btn-danger-primary-focused':   colors.red[100],
+	'btn-danger-secondary-focused': colors.red[100],
+	'btn-danger-ghost-focused':     colors.red[100],
+
+	'btn-danger-stroke-rest':     colors.red[500],
+	'btn-danger-stroke-focused':  colors.red[100],
+	'btn-danger-stroke-disabled': colors.gray[100],
+
+	// BUTTON - WARNING
+	'btn-warning-primary-focused':   colors.amber[200],
+	'btn-warning-secondary-focused': colors.amber[200],
+	'btn-warning-ghost-focused':     colors.amber[200],
+
+	'btn-warning-stroke-rest':     colors.amber[500],
+	'btn-warning-stroke-focused':  colors.amber[200],
+	'btn-warning-stroke-disabled': colors.gray[100],
 };
